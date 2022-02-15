4b1af327a8ac94cd3db0c84dbd5b105362add9fb
melmerych <98534828+melmerych@users.noreply.github.com>
2022-02-15 16:37:56 +0100

    Po zmianie

---
+++ poprawionezadanie.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+
+int bezwzgledna(int n)
+
+
+{
+	if (n < 0)
+	{
+		n = n * (-1);
+	}
+	return n;
+}
+
+
+
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << "Wartosc bezwzgledna liczby: " << n << " to: " << bezwzgledna(n);
+	return 0;
+}
